37c585f2648dd22326e5ac67f0ce676cc09c60b5
omosehin ifeyemi <ifeyemidavid@gmail.com>
2025-11-21 12:49:17 +0100

    python

---
+++ operation.py
@@ -0,0 +1,52 @@
+x = 10
+y = 3
+
+print("sum:", x + y)
+print("difference:", x - y)
+print("product:", x * y)
+print("division:", x / y)
+print("floor division:", x // y)
+print("modulus:", x % y)
+print("exponential:", x ** y)
+
+# comparison operator
+
+a = 7
+b = 5
+
+print( a == b)
+print( a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+# logical operator
+
+x = 8
+y = 12
+
+print( x > 5 and y < 15)
+print(x > 10 and y == 12)
+print(not (x == y))
+
+# operator precedence practice
+
+print( 3 + 4 * 2)
+print((3 + 4) * 2)
+print(10 - 3 ** 2)
+print(10 - (3 ** 2))
+
+# using assignment operators
+
+count = 10
+count += 5
+print(count)
+count -= 3
+print(count)
+count *= 2
+print(count)
+count /= 4
+print(count)
+count %= 5
+print(count)
